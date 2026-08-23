408aade6787226f99e1fb6c94d6dd673349f83c9
Unai Villar <unaidevcoding@gmail.com>
2026-08-23 20:28:17 +0200

    feat: se desarollan metodos index, show, store y destroy para controlador de Messages

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $per_page = $request->query("per_page", 5);
+        $page = $request->query("page", 0);
+        $offset = ($page * $per_page) - 1;
+
+        $messages = Message::skip($offset)
+            ->take($per_page)
+            ->get();
+
+        return response()->json(["data" => $messages]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $message = Message::create($request->all());
+        return response()->json(["data" => $message], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $message = Message::find($id);
+
+        if (is_null($message)) {
+            return response()->json(["error" => "Message not found!"], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json(["data" => $message]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Message::destroy($id);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get("/", fn() => ["message" => "AtlasBlog is running!"]);
 
 Route::apiResource("/categories", CategoryController::class);
+Route::apiResource("/messages", MessageController::class);
