59fa55b0927d4b5f8f65010bb342073036884cea
Unai Villar <unaidevcoding@gmail.com>
2026-08-25 10:05:10 +0200

    feat: se agrega columan deleted_at modelos Cateogory y Message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ["content", "post_id"];
     protected $hidden = ["created_at", "updated_at"];
 
